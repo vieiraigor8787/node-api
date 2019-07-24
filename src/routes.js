b345dd616c6a488cda4c2e1a0dae434d78bcea24
vieiraigor8787 <igor_vc@hotmail.com>
2019-07-24 15:34:57 -0300

    finalizando reajustes

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,8 @@
 const express = require('express')
 const routes = express.router()
 
-//primeira rota
-routes.get('/', (req, res) => {
-    // Product.creat({
-    //     title: 'react native',
-    //     description: 'build native apps with React',
-    //     url: 'https://github.com/facebook/react-native'
-    // })
-        
-    return res.send('hello ergow')
-})
+const ProductController = require('../ProductController')
+
+routes.get("/products", ProductController.index)
 
 module.exports = routes
