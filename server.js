9c0a6771fae5f177f54b1a1f1df7ffca70dd2849
vieiraigor8787 <igor_vc@hotmail.com>
2019-07-24 15:19:03 -0300

    model produto

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const requireDir = require('require-dir')
 
+//iniciando o app
 const app = express()
 
-// iniciando o DB
+//iniciando o DB
 mongoose.connect(
     "mongodb://localhost:27017/nodeapi",
     { useNewUrlParser: true }
 )
+//registrando o modulo product
+requireDir('./src/models')
 
+//primeira rota
 app.get('/', (req, res) => {
-    res.send('hello ergow')
+    Product.creat({
+        title: 'react native',
+        description: 'build native apps with React',
+        url: 'https://github.com/facebook/react-native'
+    })
+        
+    return res.send('hello ergow')
 })
 
 app.listen(3001)
