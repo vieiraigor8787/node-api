123edf753612318856d60b7d2649e5d52b758daa
vieiraigor8787 <igor_vc@hotmail.com>
2019-07-24 17:14:10 -0300

    cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const requireDir = require('require-dir')
+const requireDir = require('require-dir');
+const cors = require('cors');
 
 //iniciando o app
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 //iniciando o DB
 mongoose.connect(
